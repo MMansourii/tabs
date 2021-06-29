1f5291cda4ed4cadbda23b2acd66500cbb01a4ce
Mohammad <57011800+Mammad10@users.noreply.github.com>
2021-06-29 09:05:27 +0300

    Updates

---
+++ precache-manifest.5ed5a2725031cfcb7ebfd81d443c079c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2781cced6cc7a41fc5d75dfd77a2cd9d",
+    "url": "/tabs/index.html"
+  },
+  {
+    "revision": "05c267c721e86061f78e",
+    "url": "/tabs/static/css/main.77ad5ced.chunk.css"
+  },
+  {
+    "revision": "1fd6ce2224cf0d523c9a",
+    "url": "/tabs/static/js/2.bcafc819.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tabs/static/js/2.bcafc819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05c267c721e86061f78e",
+    "url": "/tabs/static/js/main.2522f154.chunk.js"
+  },
+  {
+    "revision": "c99201bb389f8c23b139",
+    "url": "/tabs/static/js/runtime-main.e9ac576b.js"
+  }
+]);
